04e13f66cedcc1161294a7f152a292763d91b28b
Ramli <29719586+mabere@users.noreply.github.com>
2025-06-06 21:15:06 +0800

    Update 2025_05_31_140221_create_laporan_kesalahan_table.php

--- database/migrations/2025_05_31_140221_create_laporan_kesalahan_table.php
+++ database/migrations/2025_05_31_140221_create_laporan_kesalahan_table.php
@@ -12,6 +12,8 @@ return new class extends Migration {
     {
         Schema::create('laporan_kesalahan', function (Blueprint $table) {
             $table->id();
+            $table->string('pelapor_id')->nullable();
+            $table->string('pelapor_type')->nullable();
             $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('set null');
             $table->foreignId('kata_id')->constrained('kata')->onDelete('cascade');
             $table->enum('tipe_kesalahan', ['terjemahan', 'ejaan', 'arti', 'contoh_kalimat', 'media', 'lainnya']);
